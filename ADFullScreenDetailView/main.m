bbef44eee245e0410bd92a2e5aca98fc8d0c9635
Zack Liston <zackmliston@gmail.com>
2014-01-14 14:54:03 -0500

    Initial Commit

---
+++ ADFullScreenDetailView/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ADFullScreenDetailView
+//
+//  Created by Zack Liston on 1/14/14.
+//  Copyright (c) 2014 agilemd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "ADAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([ADAppDelegate class]));
+    }
+}
---
+++ ADFullScreenDetailViewTests/ADFullScreenDetailViewTests.m
@@ -0,0 +1,34 @@
+//
+//  ADFullScreenDetailViewTests.m
+//  ADFullScreenDetailViewTests
+//
+//  Created by Zack Liston on 1/14/14.
+//  Copyright (c) 2014 agilemd. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface ADFullScreenDetailViewTests : XCTestCase
+
+@end
+
+@implementation ADFullScreenDetailViewTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
